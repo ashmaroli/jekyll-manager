aff79bc384f9a08384d71f2278031641c78a2d44
Ashwin Maroli <ashmaroli@gmail.com>
2020-03-12 22:44:45 +0530

    Migrate to ducks model of creating reducers

---
+++ src/ducks/index.js
@@ -0,0 +1,30 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import config from './config';
+import dashboard from './dashboard';
+import pages from './pages';
+import collections from './collections';
+import metadata from './metadata';
+import drafts from './drafts';
+import datafiles from './datafiles';
+import staticfiles from './staticfiles';
+import templates from './templates';
+import theme from './theme';
+import utils from './utils';
+import notifications from './notifications';
+
+export default combineReducers({
+  routing: routerReducer,
+  config,
+  dashboard,
+  pages,
+  collections,
+  metadata,
+  drafts,
+  datafiles,
+  staticfiles,
+  templates,
+  theme,
+  utils,
+  notifications,
+});
